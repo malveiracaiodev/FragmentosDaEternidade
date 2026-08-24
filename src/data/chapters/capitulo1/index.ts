5034379e5eb9292c387aa18bc7c57a3e858c3816
MalveiraCaioDev <caiomelo159@gmail.com>
2026-08-24 18:26:22 -0300

    Change chapter status from 'unlocked' to 'available'

--- src/data/chapters/capitulo1/index.ts
+++ src/data/chapters/capitulo1/index.ts
@@ -5,7 +5,7 @@ import { chap1cena2 } from "./cena2";
 export const capitulo1: Chapter = {
   id: "capitulo-1",
   number: 1,
-  status: "unlocked",
+  status: "available",
   title: "O Despertar",
   description:
     "O início da jornada de Caleb em um mundo esquecido, onde os Fragmentos começam a despertar.",
