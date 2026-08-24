e9e43ac86805a8d0951bcb81d1f928cfc4c94eab
MalveiraCaioDev <caiomelo159@gmail.com>
2026-08-24 18:23:48 -0300

    Refactor capitulo1 structure and formatting

--- src/data/chapters/capitulo1/index.ts
+++ src/data/chapters/capitulo1/index.ts
@@ -1,27 +1,16 @@
 import type { Chapter } from "../../../types/chapter";
-
 import { chap1cena1 } from "./cena1";
 import { chap1cena2 } from "./cena2";
 
-
 export const capitulo1: Chapter = {
-
   id: "capitulo-1",
-
-
+  number: 1,
+  status: "unlocked",
   title: "O Despertar",
-
-
   description:
     "O início da jornada de Caleb em um mundo esquecido, onde os Fragmentos começam a despertar.",
-
-
   scenes: [
-
     chap1cena1,
-
     chap1cena2,
-
   ],
-
 };
